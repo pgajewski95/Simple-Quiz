fe87d70e3b960e977e59936c240e29b7db9684a8
pgajewski95 <pgajewski95@wp.pl>
2019-03-13 12:54:15 +0100

    add html

--- src/main/java/com/example/quiz/Controller/TestController.java
+++ src/main/java/com/example/quiz/Controller/TestController.java
@@ -1,8 +1,12 @@
 package com.example.quiz.Controller;
 
+import com.example.quiz.Model.Test;
 import com.example.quiz.Model.User;
+import com.example.quiz.Repository.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,13 +17,18 @@ public class TestController {
 
 
     private User user = new User();
-    @ResponseBody
-    @GetMapping("/tests/{id}/play")
-    public String play(@PathVariable Integer id){
-    user.addPoints();
-    return ""+ user.getNumberOfPoints();
 
-}
+    @Autowired
+    private TestRepository testRepository;
+    @GetMapping("/tests/{id}/play")
+    public String play(@PathVariable Integer id, ModelMap modelMap) {
+        Test test = testRepository.findById(id).get();
+        modelMap.addAttribute("test", test);
+        modelMap.addAttribute("question", test.getQuestionList().get(0));
+        return "test";
+
+
+    }
 
 
 }
