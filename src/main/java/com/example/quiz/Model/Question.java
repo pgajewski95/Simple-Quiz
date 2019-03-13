6f9a9a348e5b2006717a3ae905f500de0f8119fb
pgajewski95 <pgajewski95@wp.pl>
2019-03-13 10:40:20 +0100

    second commit

--- src/main/java/com/example/quiz/Model/Question.java
+++ src/main/java/com/example/quiz/Model/Question.java
@@ -1,4 +1,56 @@
 package com.example.quiz.Model;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import javax.persistence.*;
+
+@Entity
 public class Question {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private String content;
+
+    private Boolean correct;
+    @ManyToOne
+    public Test test;
+
+    public Test getTest() {
+        return test;
+    }
+
+    public void setTest(Test test) {
+        this.test = test;
+    }
+    public Question(){}
+
+    public Question(String content, Boolean correct) {
+        this.content = content;
+        this.correct = correct;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Boolean getCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(Boolean correct) {
+        this.correct = correct;
+    }
 }
--- src/main/java/com/example/quiz/Model/Test.java
+++ src/main/java/com/example/quiz/Model/Test.java
@@ -1,4 +1,46 @@
 package com.example.quiz.Model;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+@Entity
 public class Test {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    //nazwa klucza obcego po drugiej stronie
+    @OneToMany(mappedBy = "test")
+    private List<Question> questionList = new ArrayList<>();
+
+    public List<Question> getQuestionList() {
+        return questionList;
+    }
+
+    public void setQuestionList(List<Question> questionList) {
+        this.questionList = questionList;
+    }
+
+    private String name;
+
+    public Test(){}
+
+    public Test(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
